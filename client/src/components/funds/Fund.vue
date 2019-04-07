7b89694afafbfb8d17dda307a121593aac047ec5
Rob Bowen <robinbristoluk@yahoo.co.uk>
2019-04-07 14:14:56 +0100

    Add ability to sell holdings

--- client/src/components/funds/Fund.vue
+++ client/src/components/funds/Fund.vue
@@ -2,12 +2,10 @@
   <div class="fund">
     <div class="fund__row">
       <div class="fund__chart">
-        <fund-percentage-chart
-          :percentage-of-portfolio="fund.percentageOfPortfolio"
-        />
+        <fund-percentage-chart :percentage-of-portfolio="fund.percentageOfPortfolio"/>
       </div>
       <div class="fund__details">
-        <fund-details :fund="fund" />
+        <fund-details :fund="fund"/>
       </div>
     </div>
     <div class="fund__panels">
@@ -18,23 +16,32 @@
             toggleOn: showChart
           }"
           @click="toggleChart"
-        >
-          Show Trend Chart
-        </button>
+        >Show Trend Chart</button>
         <button
           class="btn"
           :class="{
             toggleOn: showInvest
           }"
           @click="toggleInvest"
-        >
-          Invest
-        </button>
+        >Invest</button>
+        <button
+          class="btn"
+          :class="{
+            toggleOn: showRaise
+          }"
+          @click="toggleRaise"
+        >Raise</button>
       </div>
       <transition name="slide" mode="out-in">
-        <fund-history-chart :history="fund.history" v-if="showChart" />
+        <fund-history-chart :history="fund.history" v-if="showChart"/>
         <fund-invest
           v-if="showInvest"
+          :portfolioName="portfolioName"
+          :accountName="accountName"
+          :fund="fund"
+        />
+        <fund-raise
+          v-if="showRaise"
           :portfolioName="portfolioName"
           :accountName="accountName"
           :fund="fund"
@@ -49,13 +56,15 @@ import FundHistoryChart from "@/components/funds/FundHistoryChart";
 import FundPercentageChart from "@/components/funds/FundPercentageChart";
 import FundDetails from "@/components/funds/FundDetails";
 import FundInvest from "@/components/funds/FundInvest";
+import FundRaise from "@/components/funds/FundRaise";
 import AccountVue from "../portfolio/Account.vue";
 export default Vue.extend({
   components: {
     FundHistoryChart,
     FundPercentageChart,
     FundDetails,
-    FundInvest
+    FundInvest,
+    FundRaise
   },
   props: {
     fund: {
@@ -74,16 +83,24 @@ export default Vue.extend({
   data() {
     return {
       showChart: false,
-      showInvest: false
+      showInvest: false,
+      showRaise: false
     };
   },
   methods: {
     toggleChart() {
       this.showChart = !this.showChart;
       this.showInvest = false;
+      this.showRaise = false;
     },
     toggleInvest() {
       this.showInvest = !this.showInvest;
+      this.showChart = false;
+      this.showRaise = false;
+    },
+    toggleRaise() {
+      this.showRaise = !this.showRaise;
+      this.showInvest = false;
       this.showChart = false;
     }
   }
---
+++ client/src/components/funds/FundRaise.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="fund__invest">
+    <p>Please enter the number of units you wish to sell.</p>
+    <FormField
+      id="units"
+      type="number"
+      :validationError="validationError"
+      v-model="amount"
+      labelText="Number of units"
+      :narrow="true"
+    ></FormField>
+    <button class="btn" @click="handleClick">Raise</button>
+    <p class="error" v-if="submitError">{{ submitError }}</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import FormField from "@/components/FormField";
+import { showSuccessToast, showErrorToast } from "@/services/toastSvc";
+import { placeTrade } from "@/services/portfolioSvc";
+import { Trade } from "@/types/funds";
+export default Vue.extend({
+  components: {
+    FormField
+  },
+  props: {
+    portfolioName: {
+      type: String,
+      required: true
+    },
+    accountName: {
+      type: String,
+      required: true
+    },
+    fund: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      submitError: "",
+      validationError: "",
+      amount: "0"
+    };
+  },
+  methods: {
+    validateForm(): boolean {
+      let hasErrors = false;
+      this.validationError = "";
+      this.submitError = "";
+
+      const num = parseInt(this.amount, 10);
+
+      const maxToSell = Math.floor(this.fund.quantity);
+
+      alert("maxToSell:" + maxToSell);
+
+      if (isNaN(num) || num <= 0 || num > maxToSell) {
+        hasErrors = true;
+        this.validationError = `Please enter a valid whole number greater than 0 and less than or equal to ${maxToSell}`;
+      } else {
+        // This will remove any e's  as otherwise 1e etc is valid
+        this.amount = num.toString();
+      }
+
+      return !hasErrors;
+    },
+    async handleClick(): Promise<void> {
+      if (this.validateForm()) {
+        try {
+          const minusTrade = -parseInt(this.amount, 10);
+          alert(minusTrade);
+          const trade: Trade = {
+            fundName: this.fund.isin,
+            amount: minusTrade
+          };
+          await placeTrade(this.portfolioName, this.accountName, trade);
+          // Handle successful login
+          showSuccessToast("Trade posted ok!");
+          // Clear the form down
+          this.amount = "";
+        } catch (e) {
+          this.submitError =
+            "Sorry, something has gone wrong. Please try again later.";
+          showErrorToast("Something has gone wrong!");
+        }
+        return;
+      }
+
+      showErrorToast("Invalid amount entered!");
+    }
+  }
+});
+</script>
+<style scoped>
+.fund__invest {
+  padding: 20px;
+}
+p.error {
+  color: red;
+  margin-top: 20px;
+  margin-bottom: 0;
+}
+</style>
